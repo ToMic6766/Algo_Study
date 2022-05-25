3215101c1d65f8e323975a1d342c61a248cf5c38
SungJin <tjdwls14690@naver.com>
2022-05-26 01:50:40 +0900

    05-26

---
+++ code_up/code_up_7.py
@@ -0,0 +1,6 @@
+num1,num2 = map(int,input().split(" "))
+print(num1 + num2)
+
+# a,b = input().split()
+# c = int(a) + int(b)
+# print(c)
---
+++ code_up/code_up_8.py
@@ -0,0 +1,3 @@
+num = input()
+x_num = int(num,16)
+print("%o"% x_num)
